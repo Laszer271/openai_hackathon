d058feb556e3312178a052f193abe087d39f3adb
Wiktor <wiktor.michalski.2@gmail.com>
2022-12-15 20:11:37 +0100

    added browsable predictions

--- main.py
+++ main.py
@@ -13,20 +13,34 @@ def gift_idea_to_amazon(idea: str):
 
     return url
 
+def update_image(interests: str, gender: str, age: str, occasion: str, gift_ideas, index):
+    img_gen = ImageGenerator()
+
+    prompt_data = make_prompt_data_for_gpt(
+            interests, gender, int(age), occasion)
+
+    if isinstance(gift_ideas, list):
+        output_image = img_gen.generate_image(gift_ideas[index], prompt_data)
+    else:
+        output_image = img_gen.generate_image('burning computer', prompt_data)
+
+    return output_image
+
+
 def generate_text_and_image(interests: str, gender: str, age: str, occasion: str):
     img_gen = ImageGenerator()
     text_gen = TextGenerator()
 
     output_text = text_gen.generate_text(interests, gender, age, occasion)
     gift_ideas = [idea for idea in re.split('[0-9]|\.', output_text.replace("\n", "")) if idea]
-    
+
     prompt_data = make_prompt_data_for_gpt(
             interests, gender, int(age), occasion)
 
     output_image = img_gen.generate_image(gift_ideas[0], prompt_data)
 
     if text_gen.is_done and img_gen.is_done:
-        return output_text, output_image
+        return gift_ideas, output_image
     else:
         print(output_image)
         print(output_image)
@@ -47,13 +61,28 @@ if __name__ == '__main__':
                               options=['Woman', 'Man', 'Non-binary'], index=0)
 
         interests = sel_col.text_input(
-            'Describe the person interests', placeholder='Likes swimming and car racing', max_chars=2000)
+            'Describe the person interests', placeholder='swimming, car racing', max_chars=2000)
 
         should_generate = sel_col.button('Generate')
 
+        should_update = disp_col.button('Need another idea?')
+
+        if 'count' not in st.session_state:
+            st.session_state.count = 0
+
         if should_generate:
-            output_text, output_image = generate_text_and_image(interests, gender, age, occasion)
-            disp_col.write(output_text)
+            st.session_state.ideas, output_image = generate_text_and_image(interests, gender, age, occasion)
+            disp_col.write(f'What about... {st.session_state.ideas[0].upper()}?')
             disp_col.image(output_image)
+        
+        if should_update:
+            st.session_state.count += 1
 
-
+            if st.session_state.count < 1:
+                output_image = update_image(interests, gender, age, occasion, st.session_state.ideas, st.session_state.count)
+                disp_col.write(f'What about... {st.session_state.ideas[st.session_state.count].upper()}?')
+                disp_col.image(output_image)
+            else: 
+                output_image = update_image(interests, gender, age, occasion, 'sad face', st.session_state.count)
+                disp_col.write(f'I have run out of ideas :(')
+                disp_col.image(output_image)
